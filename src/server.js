78a88b32cd23ceb9a69e724a81ee3c867697ff51
gkehgl1 <gkehgl2@naver.com>
2019-10-26 16:27:22 +0900

    delete greetings

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
-"scripts": {
-  "dev" : "nodemon --exec babel-node src/server.js"
-}
+require("dotenv").config()
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({
+  schema
+});
+
+server.express.use(logger("dev"));
+
+
+server.start({port: PORT}, () => 
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
