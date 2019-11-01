d8a610fc40abc9163da3c280681d7d86dbcceb8f
gkehgl1 <gkehgl2@naver.com>
2019-11-01 16:41:17 +0900

    fragment kill

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,30 +2,34 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Post: {
-    isLiked: async(parent, _, {request}) => {
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
+    likes: ({ id }) => prisma.post({ id }).likes(),
+    isLiked: (parent, _, { request }) => {
       const { user } = request;
       const { id } = parent;
       return prisma.$exists.like({
-        AND :[
+        AND: [
           {
             user: {
               id: user.id
             }
           },
           {
-            post : {
+            post: {
               id
             }
           }
         ]
       });
     },
-    likeCount: parent => 
+    likeCount: parent =>
       prisma
         .likesConnection({
-          where: {post : {id: parent.id}}
+          where: { post: { id: parent.id } }
         })
         .aggregate()
         .count()
   }
-}
+};
